8808904ab6c4fc4778e8080c273e9d6e5f824805
Jan Prchal <prchal.jan@outlook.com>
2019-06-04 12:24:50 +0100

    now the nav opens by clicking anywhere

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 "use strict";
 
+console.log(document.body);
+const states = {
+  nav: false,
+}
 
-
-
-
+document.body.addEventListener('click', function() {
+  this.classList.toggle('nav-open');
+  // if(states.nav === false) {
+  //   document.body.classList.add('nav-open');
+  //   states.nav = true;
+  // } else {
+  //   document.body.classList.remove('nav-open');
+  //   states.nav = false;
+  // }
+  
+  console.log('body click event occured');
+})
 
