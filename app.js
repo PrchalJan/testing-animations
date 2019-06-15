fbae66064b2f394620d4be2fe33428a6d06e5d27
Jan Prchal <prchal.jan@outlook.com>
2019-06-15 19:45:17 +0100

    complicated solution

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-console.log(document.body);
-// document.body.style.backgroundColor = 'red';
+
 // Detect swipe:
 document.addEventListener('touchstart', handleTouchStart, false);
-document.addEventListener('touchmove', handleTouchMove, false);
+document.addEventListener('touchmove', function(e) {
+  handleTouchMove(e, 
+    function() {
+    document.body.style.backgroundColor = 'orange';
+  }, function() {
+    document.body.style.backgroundColor = 'brown';
+  }, function() {
+    document.body.style.backgroundColor = 'black';
+  }, function() {
+    document.body.style.backgroundColor = 'goldenrod';
+  })
+}, false);
 
 var xDown = null;
 var yDown = null;
 
 function getTouches(e) {
-  return e.touches;
+  return e.touches ||              // browser API
+         e.originalEvent.touches;  // jQuery
 }
 
 function handleTouchStart(e) {
@@ -17,7 +28,7 @@ function handleTouchStart(e) {
   yDown = firstTouch.clientY;
 };
 
-function handleTouchMove(e) {
+function handleTouchMove(e, left, right, up, down) {
   if(!xDown || !yDown) {
     return;
   };
@@ -31,21 +42,21 @@ function handleTouchMove(e) {
   if(Math.abs(xDiff) > Math.abs(yDiff)) {
     if(xDiff > 0) {
       // Left Swipe
-      document.body.style.backgroundColor = 'red';
+      left()
     } else {
       //Right Swipe
-      document.body.style.backgroundColor = 'green';
+      right()
     }
   } else {
     if(yDiff > 0) {
       // Up Swipe
-      document.body.style.backgroundColor = 'blue';
+      up()
     } else {
       // Down Swipe
-      document.body.style.backgroundColor = 'yellow';
+      down()
     }
   }
-
+  // reset values
   xDown = null;
   yDown = null;
 
